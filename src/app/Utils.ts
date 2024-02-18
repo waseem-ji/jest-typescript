675bbb18d8d58a8a40405011c5889bdb3f13336b
waseem-ji <waseem@mozilor.com>
2024-02-18 21:13:11 +0530

    Jest hooks and parameterized tests

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -1,3 +1,26 @@
 export function toUpperCase(arg: string) {
     return arg.toUpperCase();
 }
+export type StringInfo = {
+    lowerCase: string,
+    uppercase: string,
+    charactors: string[],
+    length: number,
+    extraInfo: {}
+}
+
+export function getStringInfo(arg: string) :StringInfo {
+    return {
+        lowerCase:arg.toLowerCase(),
+        uppercase:arg.toUpperCase(),
+        charactors: Array.from(arg),
+        length: arg.length,
+        extraInfo: {}
+    }
+}
+
+export class StringUtils {
+    public toUpperCase(arg: string) {
+        return arg.toUpperCase();
+    }
+}
--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,6 +1,41 @@
-import { toUpperCase } from "../app/Utils";
+import { StringUtils, getStringInfo, toUpperCase } from "../app/Utils";
 
 describe('Utils test suite',()=> {
+
+    describe('StringUtils test suite', () => {
+
+        let sut: StringUtils;
+
+        beforeEach(()=>{
+            sut = new StringUtils();
+            console.log('setup');
+            
+        })
+
+        afterEach(()=>{
+            console.log('after each - teardown');
+            
+        })
+
+
+        it.only('should return correct uppercase for valid string' ,() => {
+
+            const actual = sut.toUpperCase('abc');
+
+            expect(actual).toBe('ABC');
+            console.log('Actual test');
+            
+        })
+    })
+
+
+
+
+
+
+
+
+
     it('should return Uppercase for a valid string' , () => {
         //arrange
         const sut = toUpperCase;
@@ -13,4 +48,45 @@ describe('Utils test suite',()=> {
         expect(actual).toBe(expected);
 
     })
+
+    it.each([
+        {input:"abc" ,expected:"ABC"},
+        {input:"my-String" ,expected:"MY-STRING"},
+        {input:"xyz" ,expected:"XYZ"}
+    ])('$input toUpperCase should return $expected', ({input,expected}) => {
+        const sut = toUpperCase;
+
+        const actual = sut(input);
+
+        expect(actual).toBe(expected);
+    })
+
+    describe('getStringInfo fo arg My-string should ', () => {
+        test('return right length', () => {
+            const actual = getStringInfo('My-string');
+            expect(actual.charactors).toHaveLength(9);
+        })
+        test('return uppercase elements' , ()=>{
+            const actual = getStringInfo('My-string');
+            expect(actual.uppercase).toBe('MY-STRING');
+        })
+        test('return right charactors' , ()=>{
+            const actual = getStringInfo('My-string');
+            expect(actual.charactors).toContain<string>('M');
+            expect(actual.charactors).toEqual(['M','y','-','s','t','r','i','n','g']);
+            expect(actual.charactors).toEqual(
+                expect.arrayContaining(['y','-','s','M','t','r','i','n','g'])
+            );
+        })
+        test('return defined extra info', ()=>{
+            const actual = getStringInfo('My-string');
+            expect(actual.extraInfo).not.toBe(undefined);
+            expect(actual.extraInfo).not.toBeUndefined();
+            expect(actual.extraInfo).toBeDefined();
+            expect(actual.extraInfo).toBeTruthy();
+            expect(actual.extraInfo).toEqual({});
+
+        })
+    })
 })
+
