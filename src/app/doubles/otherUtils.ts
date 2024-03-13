a5546e4e6cbe4ded4b9df42ee84ab1b676763003
waseem-ji <waseem@mozilor.com>
2024-03-13 12:19:35 +0530

    Feat: mocks and jestMocks mocks are used to track oru code and number of times a function has been called

--- src/app/doubles/otherUtils.ts
+++ src/app/doubles/otherUtils.ts
@@ -1,11 +1,29 @@
 export type StringInfo = {
-    lowerCase: string,
-    uppercase: string,
-    charactors: string[],
-    length: number,
-    extraInfo: Object | undefined
+  lowerCase: string;
+  uppercase: string;
+  charactors: string[];
+  length: number;
+  extraInfo: Object | undefined;
+};
+
+export function calculateComplexity(stringInfo: StringInfo) {
+  return Object.keys(stringInfo.extraInfo).length * stringInfo.length;
 }
 
-export function calculateComplexity(stringInfo: StringInfo){
-    return Object.keys(stringInfo.extraInfo).length * stringInfo.length;
+// fakes: simplified working implementation of various functionalities we use
+
+type LoggerServiceCallback = (arg: string) => void;
+
+export function toUpperCasewithCb(
+  arg: string,
+  callback: LoggerServiceCallback
+) {
+  if (!arg) {
+    callback("Invalid Argument");
+    return;
+  }
+  callback(`Callback triggered with arg ${arg}`);
+  return arg.toUpperCase();
 }
+
+//Mocks or mockObject does two things - It tracks the arguments for a method that is called and also it tracks the times it is called.
--- src/test/doubles/otherUtils.test.ts
+++ src/test/doubles/otherUtils.test.ts
@@ -1,15 +1,87 @@
-import { calculateComplexity } from "../../app/doubles/otherUtils"
-describe("Other Utils test suite" , () => {
-    it('should return complexity of 10 for the test stub' , ()=>{
-        const stubInfo = {
-            length : 5,
-            extraInfo : {
-                field1 : 'info1',
-                field2 : 'info2'
-            }
-        }
+import {
+  calculateComplexity,
+  toUpperCasewithCb,
+} from "../../app/doubles/otherUtils";
 
-        const actual = calculateComplexity(stubInfo as any);
-        expect(actual).toBe(10);
-    })
-})
+describe("Other Utils test suite", () => {
+  it("should return complexity of 10 for the test stub", () => {
+    const stubInfo = {
+      length: 5,
+      extraInfo: {
+        field1: "info1",
+        field2: "info2",
+      },
+    };
+
+    const actual = calculateComplexity(stubInfo as any);
+    expect(actual).toBe(10);
+  });
+
+  it("should return void for invalid argument", () => {
+    const actual = toUpperCasewithCb("", () => {});
+
+    expect(actual).toBeUndefined();
+  });
+
+  it("should return uppercase for valid argument", () => {
+    const actual = toUpperCasewithCb("abcde", () => {});
+
+    expect(actual).toBe("ABCDE");
+  });
+
+  describe("tracking callbacks", () => {
+    let cbArgs = [];
+    let timesCalled = 0;
+
+    function callBackMock(arg: string) {
+      cbArgs.push(arg);
+      timesCalled++;
+    }
+
+    afterEach(() => {
+      cbArgs = [];
+      timesCalled = 0;
+    });
+
+    it("should return void for invalid argument", () => {
+      const actual = toUpperCasewithCb("", callBackMock);
+
+      expect(actual).toBeUndefined();
+      expect(cbArgs).toContain("Invalid Argument");
+      expect(timesCalled).toBe(1);
+    });
+
+    it("should return uppercase args for valid argument", () => {
+      const actual = toUpperCasewithCb("abc", callBackMock);
+
+      expect(actual).toBe("ABC");
+      expect(cbArgs).toContain("Callback triggered with arg abc");
+      expect(timesCalled).toBe(1);
+    });
+  });
+
+  describe("tracking callbacks with jest", () => {
+    let callBackMock = jest.fn();
+    afterEach(() => {
+      callBackMock.mockClear();
+    });
+
+    it("should return void for invalid argument", () => {
+      const actual = toUpperCasewithCb("", callBackMock);
+
+      expect(actual).toBeUndefined();
+      expect(callBackMock).toHaveBeenCalledWith("Invalid Argument");
+      expect(callBackMock).toHaveBeenCalledTimes(1);
+    });
+
+    it("should return uppercase args for valid argument", () => {
+      const actual = toUpperCasewithCb("abc", callBackMock);
+
+      expect(actual).toBe("ABC");
+      expect(callBackMock).toHaveBeenCalledWith(
+        "Callback triggered with arg abc"
+      );
+      expect(callBackMock).toHaveBeenCalledTimes(1);
+    });
+  });
+});
