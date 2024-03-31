3c3feec62ec825a3abf4bc107f4eacfb1115964e
waseem-ji <waseem@mozilor.com>
2024-03-31 18:04:35 +0530

    Feat: integrated server_app

--- jest.config.ts
+++ jest.config.ts
@@ -1,19 +1,14 @@
-import type { Config } from '@jest/types'
+import type { Config } from "@jest/types";
 
-const baseDir = '<rootDir>/src/app/doubles';
-const baseTestDir = '<rootDir>/src/test/doubles'
+const baseDir = "<rootDir>/src/app/server_app";
+const baseTestDir = "<rootDir>/src/test/server_app";
 const config: Config.InitialOptions = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    verbose: true,
-    collectCoverage:true,
-    collectCoverageFrom: [
-        `${baseDir}/**/**.ts`
-    ],
-    testMatch: [
-        `${baseTestDir}/**/**.ts`
-    ],
-    
-}
+  preset: "ts-jest",
+  testEnvironment: "node",
+  verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: [`${baseDir}/**/**.ts`],
+  testMatch: [`${baseTestDir}/**/**.ts`],
+};
 
 export default config;
